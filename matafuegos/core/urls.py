c829319ebb153476e013d0d8a83aa2e43ef07530
GonCor <cortezriquelmecl@gmail.com>
2023-09-19 01:14:13 -0300

    Creacion de rutas - inicio -clientes - matafuegs

--- matafuegos/core/urls.py
+++ matafuegos/core/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index , name='index'),
     path('matafuegos/', views.matafuegos, name='matafuegos'), 
-    path('clientes/', views.clientes_listado , name='clientes_listado'),
+    path('clientes/', views.clientes_listado , name='clientes'),
     path('clientes/detalle/<str:id_cliente>', views.clientes_detalle , name='clientes_detalle')
 ]   
