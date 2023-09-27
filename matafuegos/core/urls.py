4c37938daee1cbdda203c43474e88a8809eacb09
GonCor <cortezriquelmecl@gmail.com>
2023-09-27 13:30:24 -0300

    Se crearon nuevas vistas

--- matafuegos/core/urls.py
+++ matafuegos/core/urls.py
@@ -4,10 +4,13 @@ from . import views
 
 urlpatterns = [
     path('', views.index , name='index'),
-    path('matafuegos/', views.matafuegos, name='matafuegos'), 
+    path('orden_pedido/', views.orden_pedido, name='orden_pedido'), 
+    path('orden_pedido/<str:id_pedido>', views.orden_detalle, name='orden_detalle'),
+    path('orden_procesada/', views.orden_procesada, name='orden_procesada'), 
     path('estadisticas/', views.estadisticas, name='estadisticas'), 
-    path('stock/', views.stock, name='stock'), 
+    path('estadisticas/ordenes/', views.ordenes, name='ordenes'), 
+    path('remito/', views.remito, name='remito'),  
     path('clientes/', views.clientes_listado , name='clientes'),
-    path('clientes/detalle/<str:id_cliente>', views.clientes_detalle , name='clientes_detalle'),
-    path('matafuegos/', views.matafuegos, name='matafuegos'), 
+    path('clientes/detalle/<str:id_cliente>/', views.clientes_detalle, name='clientes_detalle'),
+    
     ] 
